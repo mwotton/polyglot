7de4e5725d98df68936b05602a6cbfff8673ed47
Mark Wotton <mwotton@gmail.com>
2014-03-11 18:42:01 +1100

    remove tracing

--- Language/Polyglot.hs
+++ Language/Polyglot.hs
@@ -8,7 +8,6 @@ import           Data.Maybe    (fromMaybe)
 import           Data.Ord
 import           Data.Text     (Text)
 import qualified Data.Text     as T
-import           Debug.Trace
 
 data Hole=Hole
 
@@ -16,19 +15,17 @@ type PolyModel = Text -> [(Text, Double)]
 type Bigram = Text
 
 build :: [(Text, Text)] -> PolyModel
-build inputs = \query -> let
-  res = sortBy (comparing (neg . snd)) $
-        map (second (compute query)) normalised
-  in trace (show res) res
+build inputs = \query -> sortBy (comparing $ neg . snd) $
+                         map (second $ compute query) normalised
+
   where bigrams = map (T.take 2) . T.tails
         neg x = x * (-1)
         occurrences = map (head &&& length) . group . sort . bigrams
         normalised :: [(Text, Bigram -> Double)]
         normalised = map (second makeEval) inputs
         makeEval text = let os = occurrences text
-                            total :: Double
                             total = sum $ map (fromIntegral . snd) os
-                            dict = Map.fromList $ map (second (\l -> fromIntegral l/total)) os
+                            dict = Map.fromList $ map (second (\l -> fromIntegral l/total))  os
                         in (fromMaybe 0 . flip Map.lookup dict)
         average x = sum x / fromIntegral (length x)
         compute query evaluator = average (map evaluator $ bigrams query)
