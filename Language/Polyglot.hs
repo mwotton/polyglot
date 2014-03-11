be733c39e4222787cf54bd8b676179f395be161e
Mark Wotton <mwotton@gmail.com>
2014-03-11 18:35:59 +1100

    eh, some arrow hacking too. why not.

--- Language/Polyglot.hs
+++ Language/Polyglot.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Language.Polyglot where
 
-import           Control.Arrow ((&&&))
+import           Control.Arrow (second, (&&&))
 import           Data.List     (group, sort, sortBy)
 import qualified Data.Map      as Map
 import           Data.Maybe    (fromMaybe)
@@ -18,19 +18,19 @@ type Bigram = Text
 build :: [(Text, Text)] -> PolyModel
 build inputs = \query -> let
   res = sortBy (comparing (neg . snd)) $
-        map (\(name,func) -> (name,compute query func)) normalised
+        map (second (compute query)) normalised
   in trace (show res) res
   where bigrams = map (T.take 2) . T.tails
         neg x = x * (-1)
         occurrences = map (head &&& length) . group . sort . bigrams
         normalised :: [(Text, Bigram -> Double)]
-        normalised = map (\(name,text) -> (name, makeEval text)) inputs
+        normalised = map (second makeEval) inputs
         makeEval text = let os = occurrences text
                             total :: Double
                             total = sum $ map (fromIntegral . snd) os
-                            dict = Map.fromList $ map (\(k,l) -> (k, fromIntegral l/total)) os
+                            dict = Map.fromList $ map (second (\l -> fromIntegral l/total)) os
                         in (fromMaybe 0 . flip Map.lookup dict)
-        average x = sum x / (fromIntegral $ length x)
+        average x = sum x / fromIntegral (length x)
         compute query evaluator = average (map evaluator $ bigrams query)
 
 
